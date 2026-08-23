8ed6f9a6b16f0729d96aaf65c9627e68c2657132
Igor Beschotnov <garant.nikopol@gmail.com>
2026-08-23 22:11:15 +0300

    Wire Position.level into room queryset scoping logic

--- residents/admin.py
+++ residents/admin.py
@@ -22,15 +22,24 @@ admin.site.register(PositionAssignment, PositionAssignmentAdmin)
 class ResidentAdmin(admin.ModelAdmin):
     filter_horizontal = ('roles',)
 
+    def _get_level(self, request):
+        """Повертає рівень доступу поточного користувача (Position.Level) або None."""
+        try:
+            resident = request.user.resident_profile
+            active = resident.positionassignment_set.filter(date_to__isnull=True).first()
+            return active.position.level if active else None
+        except AttributeError:
+            return None
+
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == "room":
-            if not request.user.is_superuser:
+            level = self._get_level(request)
+            if not request.user.is_superuser and level != Position.Level.OWNER:
                 try:
                     center = request.user.resident_profile.room.center
                     kwargs["queryset"] = Room.objects.filter(center=center)
                 except (Resident.DoesNotExist, AttributeError):
                     kwargs["queryset"] = Room.objects.none()
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
-
-
+    
 admin.site.register(Resident, ResidentAdmin)
